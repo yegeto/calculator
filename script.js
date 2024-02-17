fad30497737a094de9d0ec0ab0e4c515ed2b44c8
yegeto <115154804+yegeto@users.noreply.github.com>
2024-02-18 00:00:34 +0300

    add HTML event to calculation

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
+
+let firstNum;
+let operator;
+let secondNum;
+
+const operate = (num1, operator, num2) => operator(num1, num2);
+
