c4ccb77ebe61ca917c6889406e44d782052af9d9
yegeto <115154804+yegeto@users.noreply.github.com>
2024-12-28 12:36:04 +0300

    create operate function to call math functions

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ const divide = (x, y) => x / y;
 let operandOne;
 let operandTwo;
 let operator;
+
+const operate = (operator, firstNumber, secondNumber) => {
+  switch (operator) {
+    case "+":
+      return add(firstNumber, secondNumber);
+    case "-":
+      return subtract(firstNumber, secondNumber);
+    case "*":
+      return multiply(firstNumber, secondNumber);
+    case "/":
+      return divide(firstNumber, secondNumber);
+  }
+};
